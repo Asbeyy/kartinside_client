9fba59e57026ac4ac7f9b167edc9ae45f767f8ee
federico <lacchini.federico@icloud.com>
2024-03-01 18:14:08 -0400

    core(improvements) - added doctype, split adress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,15 @@ import Selector_card from '@/components/selectorCard/selector_card'
 export default function Home() {
   return (
     <main className='landingpage'>
-      <video className='cinematic' autoPlay muted loop id="video-background">
-            <source src="./cinematic.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-        </video>
+
+        <img src="./background.jpg" className='cinematic' alt="" />
         
-      <h1 className='title-landingpage'>KART <span>IN</span> SIDE</h1>
+      {/* <h1 className='title-landingpage'>KART <span>IN</span> SIDE</h1> */}
+      
+        <img src="./logo.png" className='logo-landing' alt="" />
      
       <div className='selector-landingpage'>
         <Selector_card category='Iscriviti'page='registrazione'/>
-        <Selector_card category='Check-In' page='checkin'/>
       </div>
     </main>
   )
